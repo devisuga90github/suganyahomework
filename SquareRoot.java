b7d3beed1ca021d23ac19b99816a93f096e2d3cd
devisuga90github <147948523+devisuga90github@users.noreply.github.com>
2023-10-31 16:01:34 +0000

    Add files via upload
    Java Challenge -Day 1-Sqroot

---
+++ SquareRoot.java
@@ -0,0 +1,27 @@
+package JavaChallenge;
+
+public class SquareRoot {
+	
+	int input =26;
+	int output;
+	
+	public void calSquareRoot()
+	
+	{
+		output = (int) Math.sqrt(input);
+		System.out.println(output);
+		
+	}
+	
+
+	public static void main(String[] args) {
+		SquareRoot sqRoot =new SquareRoot();
+		sqRoot.calSquareRoot();
+	//	System.out.println("Success");
+		
+		
+		
+
+	}
+
+}
